663d7027a4904d649afeef57d701192498544740
Vitalie Spinu <spinuvit@gmail.com>
2013-12-30 20:47:16 -0800

    implement raw encoder

--- R/bencode.R
+++ R/bencode.R
@@ -1,6 +1,47 @@
 
-bencode <- function(str){
+### DECODER
+ben_decode <- function(str){
+    if(!is.character(str)) stop("str must be a character vector")
     .Call("decode1", str)
+}
+
+### ENCODER
+ben_encode <- function(obj){
+
+    enc_s <- function(str, collapse = TRUE){
+        out <- paste0(nchar(str), ":", str)
+        if(collapse) paste(out, collapse = "")
+        else out
+    }
+
+    if( class(obj) == "bendict" ){
+        nms <- enc_s(names(obj), FALSE)
+        obj <- unlist(lapply(obj, ben_encode), recursive = FALSE)
+        sprintf("d%se", paste0(nms, obj, collapse = ""))
+    } else {
+        switch(typeof(obj),
+               pairlist =,
+               list = sprintf("l%se", paste0(unlist(lapply(obj, ben_encode)), collapse = "")), 
+               integer = sprintf("l%se", paste0("i", obj, "e", collapse = "")),
+               character = sprintf("l%se", enc_s(obj)),
+               { ## default
+                   obj <- as.character(obj)
+                   if(length(obj)) sprintf("l%se", enc_s(obj))
+                   else enc_s(obj)
+               })
+    }
+}
+
+### CONSTRUCTOR
+bendict <- function(obj){
+    obj <- as.list(obj)
+    nms <- names(obj)
+    if( is.null(nms) ) stop("No names detected. Only named objects can be converted to BenDict.")
+    nms <- sort(unique(nms))
+    if(length(nms) < length(obj)) stop("Names are not unique.")
+    obj <- obj[nms]
+    class(obj) <- "bendict"
+    obj
 }
 
 setMethod("print", "bendict",
@@ -8,3 +49,4 @@ setMethod("print", "bendict",
               cat("BENDICT:\n")
               str(unclass(x), no.list = T)
           })
+
--- inst/tests/test-bencode.R
+++ inst/tests/test-bencode.R
@@ -1,20 +1,38 @@
 library(bencode)
 
-test_that("bencode parses integers, strings and lists as expected",{
-    expect_identical(bencode("li1ei2ee"), 1:2)
-    expect_identical(bencode("l2:ab3:abc4:abcde"), c("ab", "abc", "abcd"))
-    expect_identical(bencode("lli1ei2eel2:ab3:abc4:abcdee"), list(1:2, c("ab", "abc", "abcd")))
-    expect_identical(bencode("lli1ei2eli1ei2eeee"), list(list(1L, 2L, 1:2)))
-    expect_identical(bencode("lli1ei2eel1:a2:ab3:abcee"), list(1:2, c("a", "ab", "abc")))
-    expect_identical(bencode("lli1ei2eel1:a2:ab3:abci3eee"), list(1:2, list("a", "ab", "abc", 3L)))
+test_that("ben_decode parses integers, strings and lists",{
+    expect_identical(ben_decode("li1ei2ee"), 1:2)
+    expect_identical(ben_decode("l2:ab3:abc4:abcde"), c("ab", "abc", "abcd"))
+    expect_identical(ben_decode("lli1ei2eel2:ab3:abc4:abcdee"), list(1:2, c("ab", "abc", "abcd")))
+    expect_identical(ben_decode("lli1ei2eli1ei2eeee"), list(list(1L, 2L, 1:2)))
+    expect_identical(ben_decode("lli1ei2eel1:a2:ab3:abcee"), list(1:2, c("a", "ab", "abc")))
+    expect_identical(ben_decode("lli1ei2eel1:a2:ab3:abci3eee"), list(1:2, list("a", "ab", "abc", 3L)))
 })
 
-test_that("bencode parses dictionaries as expected", {
-    expect_identical(bencode("d4:abcdli1ei2ei3ee4:bbcdli1ei2ei3ei4ei5eee"),
+test_that("ben_decode parses dictionaries", {
+    expect_identical(ben_decode("d4:abcdli1ei2ei3ee4:bbcdli1ei2ei3ei4ei5eee"),
                      structure(list(abcd = 1:3, bbcd = 1:5), .Names = c("abcd", "bbcd"), class = "bendict"))
-    expect_identical(bencode("d4:abcdli1ei2ei3ee4:bbcdli1ei2ei3ei4ei5ee10:inner_dictd4:abcdli1ei2ei3ee4:bbcdli1ei2ei3ei4ei5eeee"),
+    expect_identical(ben_decode("d4:abcdli1ei2ei3ee4:bbcdli1ei2ei3ei4ei5ee10:inner_dictd4:abcdli1ei2ei3ee4:bbcdli1ei2ei3ei4ei5eeee"),
                      structure(list(abcd = 1:3, bbcd = 1:5,
                                     inner_dict = structure(list(abcd = 1:3, bbcd = 1:5),
                                         .Names = c("abcd", "bbcd"), class = "bendict")),
                                .Names = c("abcd", "bbcd", "inner_dict"), class = "bendict"))
 })
+
+test_that("encode -> decode loop works", {
+          X <- c("aaaa", "bbbb")
+          expect_identical(X, ben_decode(ben_encode(X)))
+          X <- 1:50
+          expect_identical(X, ben_decode(ben_encode(X)))
+          X <- list(1L, 2L, 3L, "abcd")
+          expect_identical(X, ben_decode(ben_encode(X)))
+          X <- list(1L, 2L, 1:3, "abcd")
+          expect_identical(X, ben_decode(ben_encode(X)))
+          X <- bendict(list(a = 1L, b = 2L, c = 1:3, d = "abcd"))
+          expect_identical(X, ben_decode(ben_encode(X)))
+          X <- bendict(list(a = 1L, b = 2L, c = 1:3, d = "abcd"))
+          expect_identical(X, ben_decode(ben_encode(X)))
+          X <- bendict(list(a = 1L, b = 2L, c = 1:3, d = "abcd",
+                            e = bendict(list(a = 1L, b = 2L, c = c("a", "b", "c"), d = "abcd"))))
+          expect_identical(X, ben_decode(ben_encode(X)))
+      })
